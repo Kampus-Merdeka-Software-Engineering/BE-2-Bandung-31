079192e6a07bc129b53ec277dcd13c9bddfda03b
syifanathaniar <140411045+syifanathaniar@users.noreply.github.com>
2023-12-05 08:18:10 +0700

    update package

---
+++ index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { prisma } = require("../config/prisma");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+
+app.get("/", async(req, res) => {
+    res.send("here is the response");
+});
+
+app.all("*", async(req, res) => {
+    res.json({
+        message: "Routes you're looking is not found",
+    });
+});
+
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is already running at ${PORT}`);
+});
